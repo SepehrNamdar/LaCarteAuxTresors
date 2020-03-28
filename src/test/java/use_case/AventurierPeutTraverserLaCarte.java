e40d5770dc4969e0feea65ec3dd41f1d4d93c5d7
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2020-03-28 18:46:29 +0100

    refactor

--- src/test/java/use_case/AventurierPeutTraverserLaCarte.java
+++ src/test/java/use_case/AventurierPeutTraverserLaCarte.java
@@ -9,9 +9,17 @@ import org.junit.jupiter.api.Test;
 
 public class AventurierPeutTraverserLaCarte {
 
+    public static final int NB_CASES_LARGEUR_CARTE = 3;
+    public static final int NB_CASES_HAUTEUR_CARTE = 4;
+
     @Test
     void pleine() {
-        Carte carte = new Carte(new Dimensions(new Largeur(3), new Hauteur(4)));
+        Largeur largeur = new Largeur(NB_CASES_LARGEUR_CARTE);
+        Hauteur hauteur = new Hauteur(NB_CASES_HAUTEUR_CARTE);
+        Dimensions dimensions = new Dimensions(largeur, hauteur);
+
+        Carte carte = new Carte(dimensions);
         Aventurier laura = new Aventurier("Laura");
+
     }
 }

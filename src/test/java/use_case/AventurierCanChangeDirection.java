21452d58a26388c3266f294e622cea4f5f447b54
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2020-04-13 14:41:08 +0200

    refactor aventurier can turn test

--- src/test/java/use_case/AventurierCanChangeDirection.java
+++ src/test/java/use_case/AventurierCanChangeDirection.java
@@ -1,14 +1,9 @@
 package use_case;
 
 import model.carte.Axe;
-import model.carte.Carte;
-import model.carte.Dimensions;
 import model.element.Aventurier;
 import org.junit.jupiter.api.Test;
 
-import static common.TestUtils.HAUTEUR_CARTE;
-import static common.TestUtils.LARGEUR_CARTE;
-import static java.util.Collections.singletonList;
 import static model.element.Orientation.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,48 +11,44 @@ public class AventurierCanChangeDirection {
 
     @Test
     void toLeft() {
-        Dimensions dimensions = new Dimensions(LARGEUR_CARTE, HAUTEUR_CARTE);
         Axe axeDepart = new Axe(1, 1);
         Aventurier laura = new Aventurier("Laura", axeDepart, NORD, "GGGG");
-        Carte carte = new Carte(dimensions, singletonList(laura));
 
-        carte.turnLeft(laura);
+        laura.turnLeft();
         assertThat(laura.getCurrentOrientation()).isEqualTo(OUEST);
         assertThat(laura.getAxe()).isEqualTo(axeDepart);
 
-        carte.turnLeft(laura);
+        laura.turnLeft();
         assertThat(laura.getCurrentOrientation()).isEqualTo(SUD);
         assertThat(laura.getAxe()).isEqualTo(axeDepart);
 
-        carte.turnLeft(laura);
+        laura.turnLeft();
         assertThat(laura.getCurrentOrientation()).isEqualTo(EST);
         assertThat(laura.getAxe()).isEqualTo(axeDepart);
 
-        carte.turnLeft(laura);
+        laura.turnLeft();
         assertThat(laura.getCurrentOrientation()).isEqualTo(NORD);
         assertThat(laura.getAxe()).isEqualTo(axeDepart);
     }
 
     @Test
     void toRight() {
-        Dimensions dimensions = new Dimensions(LARGEUR_CARTE, HAUTEUR_CARTE);
         Axe axeDepart = new Axe(1, 1);
         Aventurier laura = new Aventurier("Laura", axeDepart, NORD, "DDDD");
-        Carte carte = new Carte(dimensions, singletonList(laura));
 
-        carte.turnRight(laura);
+        laura.turnRight();
         assertThat(laura.getCurrentOrientation()).isEqualTo(EST);
         assertThat(laura.getAxe()).isEqualTo(axeDepart);
 
-        carte.turnRight(laura);
+        laura.turnRight();
         assertThat(laura.getCurrentOrientation()).isEqualTo(SUD);
         assertThat(laura.getAxe()).isEqualTo(axeDepart);
 
-        carte.turnRight(laura);
+        laura.turnRight();
         assertThat(laura.getCurrentOrientation()).isEqualTo(OUEST);
         assertThat(laura.getAxe()).isEqualTo(axeDepart);
 
-        carte.turnRight(laura);
+        laura.turnRight();
         assertThat(laura.getCurrentOrientation()).isEqualTo(NORD);
         assertThat(laura.getAxe()).isEqualTo(axeDepart);
     }
